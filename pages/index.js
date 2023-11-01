4dc661ee88c67b46622f36a021354dac60322752
ukiome <marcus@ukita.se>
2023-11-01 18:10:09 +0100

    did some alteration of the start page

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Lammfest WS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Lammfest WS" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is a workspace for the Lammfest project.
+          {/* Get started by editing <code>pages/index.js</code> */}
         </p>
       </main>
 
